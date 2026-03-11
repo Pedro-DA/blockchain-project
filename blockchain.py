926c0227498262259571988c123382a87e5b396c
Pedro-DA <pedroqalmeida04@gmail.com>
2026-03-11 15:57:30 +0000

    Flask setup

--- blockchain.py
+++ blockchain.py
@@ -1,7 +1,10 @@
 import hashlib
 import json
+from textwrap import dedent
 from time import time
 from uuid import uuid4
+
+from flask import Flask, jsonify, request
 
 class Blockchain(object):
     def __init__(self):
@@ -53,3 +56,28 @@ class Blockchain(object):
         guess = f"{lastProof}{proof}".encode()
         guessHash = hashlib.sha256(guess).hexdigest()
         return guessHash[:4]=="0000"
+    
+app = Flask(__name__)
+
+nodeIdentifier = str(uuid4()).replace("-","")
+
+Blockchain = Blockchain()
+
+@app.route("/mine", methods=["GET"])
+def mine():
+    return "We'll mine a new block"
+
+@app.route("/transactions/new",methods=["POST"])
+def newTransaction():
+    return "We'll add a new transaction"
+
+@app.route("/chain",methods=["GET"])
+def fullChain():
+    response={
+        "chain":Blockchain.chain,
+        "length":len(Blockchain.chain),
+    }
+    return jsonify(response), 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
