10a881279cb9d17c2f56e65baa48851f4599595e
Pedro-DA <pedroqalmeida04@gmail.com>
2026-03-11 15:34:47 +0000

    new blocks

--- blockchain.py
+++ blockchain.py
@@ -1,23 +1,41 @@
+import hashlib
+import json
+from time import time
+
 class Blockchain(object):
     def __init__(self):
         self.chain = []
-        self.current_transactions = []
+        self.currentTransactions = []
 
-    def new_block(self):
-        pass
+        self.newBlock(previousHash=1,proof=100)
 
-    def new_transaction(self,sender,recipient,amount):
-        self.current_transactions.append({
+    def newBlock(self,proof,previousHash=None):
+        block={
+            "index":len(self.chain)+1,
+            "timestamp":time(),
+            "transactions":self.currentTransactions,
+            "proof":proof,
+            "previousHash":previousHash or self.hash(self.chain[-1]),
+        }
+        
+        self.currentTransactions = []
+
+        self.chain.append(block)
+        return block
+
+    def newTransaction(self,sender,recipient,amount):
+        self.currentTransactions.append({
             "sender":sender,
             "recipient":recipient,
             "amount":amount,
         })
-        return self.last_block["index"] + 1
+        return self.lastBlock["index"]+1
 
     @staticmethod
     def hash(block):
-        pass
+        blockString = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(blockString).hexdigest()
 
     @property
-    def last_block(self):
-        pass
+    def lastBlock(self):
+        return self.chain[-1]
